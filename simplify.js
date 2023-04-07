5711076be7451bdb994879d517b921c0c227bb9b
vivek g <vivek13186@gmail.com>
2023-04-07 12:24:56 +0200

    commit

--- simplify.js
+++ simplify.js
@@ -1,33 +1,3 @@
-var msg = "Say hello ${msg} from ${name}";
-var code = `Say hello "vivek" from "vivek"`;
-var reg_param = /\$\{.*?\}/gm;
-var reg_code = /\".*?\"/gm;
-console.log(msg.match(reg_param))
-
-function get_params(rule) {
-    var p = rule.match(reg_param) || [];
-    p = p.map(s => s.substring(2, s.length - 1));
-    return p;
-}
-function get_code_value(rule) {
-    var p = rule.match(reg_code) || [];
-    p = p.map(s => s.substring(1, s.length - 1));
-    return p;
-}
-
-function get_hash_rule(rule) {
-    return rule.replaceAll(reg_param, "*");
-}
-
-function get_hash_code(rule) {
-    return rule.replaceAll(reg_code, "*");
-}
-console.log(get_params(msg))
-console.log(get_hash_rule(msg))
-console.log(get_hash_code(code))
-console.log(get_code_value(code))
-
-
 const CODE_REGEXP = /\".*?\"/gm;
 const RULE_REGEXP = /\$\{.*?\}/gm;
 
@@ -43,6 +13,28 @@ class Simplify {
         }else{
             console.warn(string_pattern," similar pattern is aleady registered");
         }   
+    }
+
+    eval(code){
+        code  = code.split("\n")||[];
+        for(var i=0;i<code.length;i++){
+            var lno=i+1;
+            var l = code[i];
+            try{
+               var args = this._get_code_value(l);
+               var hash  = this._get_hash_code(l);
+               if(this.rules[hash]){
+                this.rules[hash].callback(args);
+               }else{
+                throw "New rule found for : "+l;
+               }
+            }catch(e){
+                console.error("Error at line ",lno," : ",l," error ",e);
+            }
+
+        }
+        
+        
     }
     _get_rule_params(rule) {
         var p = rule.match(reg_param) || [];
